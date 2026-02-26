1defdbab15c15ae314d3a7b82b5956b34b2a4bbd
Isaac177 <isaacjogues177@gmail.com>
2026-02-26 19:20:30 +0500

    handlers

--- src/app/router.rs
+++ src/app/router.rs
@@ -5,5 +5,6 @@ use super::state::AppState;
 pub fn create_router(state: AppState) -> Router {
     Router::new()
         .route("/health/live", get(|| async { "healthy" }))
+        .route("/health/ready", get(crate::features::health::handler::ready))
         .with_state(state)
 }
---
+++ src/features/health/handler.rs
@@ -0,0 +1,4 @@
+
+pub async fn ready() -> &'static str {
+    "My app is ready, he he he he !!!"
+}
